5770610a16aae04eb8542c53aa2fa32c3616ff8a
Ddupg <ddupgs@gmail.com>
2026-04-16 19:58:42 +0800

    fix: update sha256 checksums for v0.1.1
    Change-Id: If80e1a9e9b48834af332905f24ad706c17764e9f

--- Formula/artix.rb
+++ Formula/artix.rb
@@ -7,17 +7,17 @@ class Artix < Formula
   on_macos do
     on_intel do
       url "https://github.com/ddupg/artix/releases/download/v0.1.1/artix-x86_64-apple-darwin.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "e22bfb044e7c850cbd789f20f2a74d6a7392e4e27daf1828f987cb6f6f492fb9"
     end
     on_arm do
       url "https://github.com/ddupg/artix/releases/download/v0.1.1/artix-aarch64-apple-darwin.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "e5f5276ca3f0a20156ad8bc7c4fe432f45afb59c443e7db10183459e3e9feacc"
     end
   end
 
   on_linux do
     url "https://github.com/ddupg/artix/releases/download/v0.1.1/artix-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "5d5f5da151fd0411ce571a719e2ffe1b5583a75367219470b9cf189dc1dd8d41"
   end
 
   def install
