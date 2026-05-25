03f090dc4f469241650fcf8ba7652d0ec39e7aeb
Ddupg <ddupgs@gmail.com>
2026-05-25 21:24:28 +0800

    fix: update artix formula for v0.2.0
    Change-Id: I5e2a0b89595c9b94693c54c037adc132ad00bf3d

--- Formula/artix.rb
+++ Formula/artix.rb
@@ -1,24 +1,23 @@
 class Artix < Formula
   desc "Developer workspace disk cleanup TUI with Git/worktree awareness"
   homepage "https://github.com/ddupg/artix"
-  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/ddupg/artix/releases/download/v0.1.2/artix-x86_64-apple-darwin.tar.gz"
-      sha256 "dacd1826483c85acc10ca444fee91a8528309d32ba587ab91b2ed1d99a2de1e7"
+      url "https://github.com/ddupg/artix/releases/download/v0.2.0/artix-x86_64-apple-darwin.tar.gz"
+      sha256 "5e8640a2152ed3e5126cc9108b5be3408ebc1644e639ce6e5885ab91a46aaf73"
     end
     on_arm do
-      url "https://github.com/ddupg/artix/releases/download/v0.1.2/artix-aarch64-apple-darwin.tar.gz"
-      sha256 "5949b94026791c009c7c3be5c074967375f8aa1e90feb706cd789ee9572d72c8"
+      url "https://github.com/ddupg/artix/releases/download/v0.2.0/artix-aarch64-apple-darwin.tar.gz"
+      sha256 "b3c75ed3bbc508d33ae0338c3d47c620a7fb7ff4d7b8f0bf03dc1df68789faa6"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ddupg/artix/releases/download/v0.1.2/artix-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "da7a952df0780d58c5a81f29d4f79471ea53421916a5e082b0370772c49f1234"
+      url "https://github.com/ddupg/artix/releases/download/v0.2.0/artix-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "608b5e58a7572eb0f89a9c837773d996aa72159cd8e26ac87870f00316593d75"
     end
   end
 
